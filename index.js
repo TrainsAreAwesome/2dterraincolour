ef2fd9c7ed93ccf878344d828b259154e065fc1f
Morgan <morganlote@gmail.com>
2022-10-13 14:27:22 +0100

    added the random number gen for the terrain gen

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import chalk from "chalk"
 
-console.log(chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
-console.log(chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaa") + chalk.bgWhite.hidden("ccccccccccccccccccccccccccccccccc" ) + chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+const cl = console.log
+const ch = chalk
+
+// made those easier to type ^^^
+
+// test
+//console.log(chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+//console.log(chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaa") + chalk.bgWhite.hidden("ccccccccccccccccccccccccccccccccc" ) + chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+// end of test, should make segment of the sky with a tiny cloud if run
+console.log(chalk.white.hidden("test text"))
+
+//loading screen, yeah i had to do that >>>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     i know it looks terrible
+
+cl(ch.white(" |                   |‾‾‾‾‾‾‾‾‾‾‾‾|              /\\              |‾‾‾‾‾‾‾‾‾‾‾\\        |       |\\       |        |‾‾‾‾‾‾‾‾‾‾‾‾ \n |                   |            |             /  \\             |            |               | \\      |        | \n |                   |            |            /    \\            |            |               |  \\     |        | \n |                   |            |           /      \\           |            |       |       |   \\    |        |____________  \n |                   |            |          /________\\          |            |       |       |    \\   |        |            | \n |                   |            |         /          \\         |            |       |       |     \\  |        |            | \n |                   |            |        /            \\        |            |       |       |      \\ |        |            | \n |____________       |____________|       /              \\       |___________/        |       |       \\|        |____________| "))
+
+import { terrainGen } from "./terraingen"
---
+++ terraingen.js
@@ -0,0 +1,26 @@
+export { terrainGen }
+
+// terrain gen funtion
+let terrainGen = () => {
+
+
+
+
+    let RNGResult = null
+
+
+
+
+function RNG(RNGMax) // random number gen
+{
+    return Math.floor(Math.random() * RNGMax);
+}
+
+console.log(RNG(10))
+
+}
+
+terrainGen()
+
+
+
