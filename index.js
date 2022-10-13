731bf8f904d8f74c409cb6202475c5a4180035ba
Morgan <morganlote@gmail.com>
2022-10-13 10:58:56 +0100

    got everything ready on my new pc

---
+++ index.js
@@ -0,0 +1,4 @@
+import chalk from "chalk"
+
+console.log(chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+console.log(chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaa") + chalk.bgWhite.hidden("ccccccccccccccccccccccccccccccccc" ) + chalk.bgBlue.hidden("aaaaaaaaaaaaaaaaaaaaaaaaaaa"))
